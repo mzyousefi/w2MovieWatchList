4ce09ea64baf73ecac06c6f3a5c9517a13a25701
mzyousefi <mz.yousefi112@gmail.com>
2026-02-06 01:36:42 +0330

    fix

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -1,7 +1,7 @@
 export default function SelectInput({value,placeHolder,onChangeF,children}) {
     return (
         <select className="form-control mb-2" value={value} onChange={(e)=>onChangeF(e.target.value)}>
-            ({placeHolder?<option value="">{placeHolder}</option>:null})
+            {placeHolder?<option value="">{placeHolder}</option>:null}
             {children}
         </select>
     )
